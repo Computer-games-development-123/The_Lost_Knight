d4247b8f848693faaf9c3bf9f0cb3b2f31f333eb
Itzhak Bista <itzhak173@gmail.com>
2026-01-05 18:14:55 +0200

    Fix/Update PhilipBoss behavior

--- Assets/Scripts/Bosses/PhilipBoss.cs
+++ Assets/Scripts/Bosses/PhilipBoss.cs
@@ -6,20 +6,6 @@ public class PhilipBoss : BossBase
 {
     [Header("Philip Specific")]
     public GameObject specialAttackPortal;
-    // public float slamCooldown = 4f;
-    // public int slamDamage = 15;
-    // private float lastSlamTime;
-    // private bool isSlaming = false;
-
-    // protected override void OnBossStart()
-    // {
-    //     base.OnBossStart();
-    //     bossName = "Philip";
-    //     maxHP = 150;
-    //     currentHP = maxHP;
-    //     moveSpeed = 2f;
-    //     damage = 12;
-    // }
     protected override void Start()
     {
         base.Start();
@@ -34,55 +20,11 @@ public class PhilipBoss : BossBase
     {
         //base.BossAI();
         SpecialAttack();
-        // if (Time.time >= lastSlamTime + slamCooldown && distanceToPlayer < 5f)
-        // {
-        //     StartCoroutine(SpecialAttack());
-        // }
-        // else
-        // {
-        //     base.BossAI();
-        // }
     }
 
     void SpecialAttack()
     {
-        // isSlaming = true;
-        // lastSlamTime = Time.time;
-
-        // // Jump up
-        // rb.linearVelocity = new Vector2(0, 10f);
-
-        // if (anim != null)
-        //     anim.SetTrigger("Jump");
         anim.SetTrigger("attack2");
-
-        // // Slam down
-        // rb.linearVelocity = new Vector2(0, -15f);
-
-        // yield return new WaitUntil(() => rb.linearVelocity.y == 0);
-
-        // // Create shockwave
-        // if (shockwavePrefab != null)
-        // {
-        //     Instantiate(shockwavePrefab, transform.position, Quaternion.identity);
-        // }
-
-        // // Damage player if nearby
-        // if (player != null && Vector2.Distance(transform.position, player.position) < 4f)
-        // {
-        //     PlayerHealth ph = player.GetComponent<PlayerHealth>();
-        //     if (ph != null)
-        //     {
-        //         ph.TakeDamage(slamDamage);
-        //     }
-        // }
-
-        // if (anim != null)
-        //     anim.SetTrigger("SlamImpact");
-
-        // yield return new WaitForSeconds(1f);
-
-        // isSlaming = false;
     }
 
     IEnumerator OnSpecialAttackStart()
